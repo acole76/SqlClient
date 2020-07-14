13f749cdc30d6e3c966f2dcc4409b8c2dfa28424
Anthony <acole76@gmail.com>
2020-07-14 16:07:15 -0500

    Initial

---
+++ SqlClient/ArgParse.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace ArgumentParser
+{
+  public class ArgParse
+  {
+    Dictionary<string, string> dict = new Dictionary<string, string>();
+    List<ArgItem> argList = new List<ArgItem>();
+
+    public enum ArgParseType
+    {
+      Long = 0,
+      Int = 1,
+      DateTime = 2,
+      String = 3,
+      Boolean = 4,
+      File = 5,
+      Pid = 6 //process id
+    };
+
+    public ArgParse(params ArgItem[] _argClass)
+    {
+      foreach (ArgItem arg in _argClass)
+      {
+        argList.Add(arg);
+      }
+    }
+
+    public ArgParse(List<ArgItem> _argList)
+    {
+      argList = _argList;
+    }
+
+    private ArgItem getArgItemBySwitch(string arg)
+    {
+      if (arg.StartsWith("--"))
+      {
+        foreach (ArgItem item in argList)
+        {
+          if (item.longName == arg.Substring(2))
+          {
+            return item;
+          }
+        }
+      }
+      else
+      {
+        foreach (ArgItem item in argList)
+        {
+          if (item.shortName == arg.Substring(1))
+          {
+            return item;
+          }
+        }
+      }
+
+      return null;
+    }
+
+    public void parse(string[] args)
+    {
+      string prev = null;
+      foreach (string arg in args)
+      {
+        if (arg == "-h" || arg == "--help")
+        {
+          showHelp("");
+          return;
+        }
+
+        if (arg.StartsWith("--") || arg.StartsWith("-"))
+        {
+          ArgItem item = getArgItemBySwitch(arg);
+
+          if (item == null)
+          {
+            showHelp(string.Format("Unknown switch: {0}", arg));
+            return;
+          }
+
+          prev = item.longName;
+          if(dict.ContainsKey(item.longName))
+          {
+            showHelp(string.Format("duplicate switch: {0}", item.longName));
+            return;
+          }
+
+          dict.Add(item.longName, item.defaultValue);
+        }
+        else
+        {
+          if (prev != null)
+          {
+            dict[prev] = arg;
+          }
+        }
+      }
+
+      // check is required
+      foreach (ArgItem argClass in argList)
+      {
+        if (argClass.required)
+        {
+          if (!dict.ContainsKey(argClass.longName))
+          {
+            showHelp(string.Format("Field {0} is required but is missing.", argClass.longName));
+            return;
+          }
+        }
+      }
+
+      foreach (ArgItem argClass in argList)
+      {
+
+      }
+
+      // check is valid type
+      foreach (ArgItem argClass in argList)
+      {
+        try
+        {
+          string dataType = argClass.dataType.ToString();
+          switch (argClass.argType)
+          {
+            case ArgParseType.Int:
+              Int32.Parse(dict[argClass.longName]);
+              break;
+            case ArgParseType.Long:
+              Int64.Parse(dict[argClass.longName]);
+              break;
+            case ArgParseType.DateTime:
+              DateTime.Parse(dict[argClass.longName]);
+              break;
+            case ArgParseType.Boolean:
+              bool.Parse(dict[argClass.longName]);
+              break;
+            case ArgParseType.File:
+              if (!File.Exists(dict[argClass.longName]))
+              {
+                throw new Exception("File does not exist");
+              }
+              break;
+            case ArgParseType.Pid:
+              Int32 pid = Int32.Parse(dict[argClass.longName]);
+              Process proc = Process.GetProcessById(pid);
+              if (proc == null)
+              {
+                throw new Exception("Process does not exist");
+              }
+              break;
+            default:
+              break;
+          }
+        }
+        catch (Exception e)
+        {
+          showHelp(string.Format("{0} - {1}", argClass.longName, e.Message));
+          break;
+        }
+      }
+    }
+
+    public void showHelp(string msg)
+    {
+      bool hasOptional = false;
+      if (msg.Length > 0)
+      {
+        Console.WriteLine("Error: {0}\n", msg);
+      }
+
+      Console.WriteLine("Usage: {0} <options>", AppDomain.CurrentDomain.FriendlyName);
+      Console.WriteLine("\tRequired");
+      foreach (ArgItem argClass in argList)
+      {
+        if (argClass.required == true)
+        {
+          Console.WriteLine("\t\t-{0}, --{1}\t{2}", argClass.shortName, argClass.longName, argClass.helpText);
+        }
+        else
+        {
+          hasOptional = true;
+        }
+      }
+
+      if (hasOptional)
+      {
+        Console.WriteLine("\n\tOptional");
+        foreach (ArgItem argClass in argList)
+        {
+          if (argClass.required == false)
+          {
+            Console.WriteLine("\t\t-{0}, --{1}\t{2}", argClass.shortName, argClass.longName, argClass.helpText);
+          }
+        }
+      }
+
+      if(Debugger.IsAttached)
+      {
+        Console.ReadLine();
+      }
+
+      Environment.Exit(0);
+    }
+
+    public T Get<T>(string key)
+    {
+      if (!dict.ContainsKey(key))
+        return default(T);
+
+      try
+      {
+        return (T)Convert.ChangeType(dict[key], typeof(T));
+      }
+      catch (Exception)
+      {
+        return default(T);
+      }
+    }
+  }
+
+  public class ArgItem
+  {
+    public string longName { get; set; } //double dash option --domain
+    public string shortName { get; set; } //single dash option -d
+    public string helpText { get; set; }
+    public string defaultValue { get; set; }
+    public bool required { get; set; }
+    public Type dataType { get; set; }
+    public ArgParse.ArgParseType argType { get; set; }
+
+    public ArgItem(string _longName, string _shortName, bool _required)
+    {
+      init(_longName, _shortName, _required, "", "", ArgParse.ArgParseType.String);
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText)
+    {
+      init(_longName, _shortName, _required, _helpText, "", ArgParse.ArgParseType.String);
+    }
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue)
+    {
+      init(_longName, _shortName, _required, _helpText, _defaultValue, ArgParse.ArgParseType.String);
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, ArgParse.ArgParseType _dataType)
+    {
+      init(_longName, _shortName, _required, "", "", _dataType);
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, ArgParse.ArgParseType _dataType)
+    {
+      init(_longName, _shortName, _required, _helpText, "", _dataType);
+    }
+
+    public ArgItem(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue, ArgParse.ArgParseType _dataType)
+    {
+      init(_longName, _shortName, _required, _helpText, _defaultValue, _dataType);
+    }
+
+    private void init(string _longName, string _shortName, bool _required, string _helpText, string _defaultValue, ArgParse.ArgParseType _dataType)
+    {
+      longName = _longName;
+      shortName = _shortName;
+      helpText = _helpText;
+      defaultValue = _defaultValue;
+      required = _required;
+      argType = _dataType;
+      dataType = _dataType.ToType();
+    }
+  }
+
+  public static class ArgParseExtensions
+  {
+    public static Type ToType(this ArgParse.ArgParseType argType)
+    {
+      switch (argType)
+      {
+        case ArgParse.ArgParseType.DateTime:
+          return DateTime.MaxValue.GetType();
+        case ArgParse.ArgParseType.Int:
+          return int.MaxValue.GetType();
+        case ArgParse.ArgParseType.Long:
+          return long.MaxValue.GetType();
+        case ArgParse.ArgParseType.Boolean:
+          return true.GetType();
+        default:
+          return "".GetType();
+      }
+    }
+  }
+}
+
--- SqlClient/Program.cs
+++ SqlClient/Program.cs
@@ -1,15 +1,269 @@
-﻿using System;
+﻿using ArgumentParser;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Data;
+using System.Data.Odbc;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SqlClient
 {
 	class Program
 	{
+		public static List<string> driverList = new List<string>();
+		public static Dictionary<string, string> driverDict = new Dictionary<string, string>();
+
+
 		static void Main(string[] args)
 		{
+			ArgParse argparse = new ArgParse
+			(
+					new ArgItem("driver", "d", true, "ODBC Driver: mssql,postgresql,firebird,mssql", "mssql", ArgParse.ArgParseType.String),
+					new ArgItem("username", "u", true, "username", "", ArgParse.ArgParseType.String),
+					new ArgItem("password", "p", true, "password", "", ArgParse.ArgParseType.String),
+					new ArgItem("host", "i", true, "the host name of the server", "", ArgParse.ArgParseType.String),
+					new ArgItem("catalog", "c", true, "The catalog or database", "", ArgParse.ArgParseType.String),
+					new ArgItem("sql", "s", true, "The sql statement to execute", "", ArgParse.ArgParseType.String),
+					new ArgItem("output", "o", false, "Output type: csv,json", "json", ArgParse.ArgParseType.String),
+					new ArgItem("url", "e", false, "url where data will be posted", "", ArgParse.ArgParseType.String)
+			);
+
+			argparse.parse(args);
+
+			driverDict.Add("mssql", "SQL Server");
+			driverDict.Add("mysql", "mysql");
+			driverDict.Add("firebird", "firebird");
+			driverDict.Add("postgresql", "postgresql");
+
+			string driver = argparse.Get<string>("driver");
+			string username = argparse.Get<string>("username");
+			string password = argparse.Get<string>("password");
+			string host = argparse.Get<string>("host");
+			string database = argparse.Get<string>("catalog");
+			string sql = argparse.Get<string>("sql");
+			string output = argparse.Get<string>("output");
+			string url = argparse.Get<string>("url");
+
+			ListODBCDrivers();
+
+			if(DriverExists(driver))
+			{
+				string connectionString = string.Format(getConnectionString(driver), host, database, username, password);
+
+				OdbcConnection connection = new OdbcConnection(connectionString);
+				connection.Open();
+
+				OdbcCommand cmd = new OdbcCommand(sql, connection);
+				OdbcDataReader dataReader = cmd.ExecuteReader();
+				DataTable dt = new DataTable("table");
+				dt.Load(dataReader);
+
+				string result = "";
+				if (output.ToLower() == "csv")
+				{
+					result = DataTableToCsv(dt);
+				}
+
+				if (output.ToLower() == "json")
+				{
+					result = DataTableToJson(dt);
+				}
+
+				if (url.Length == 0)
+				{
+					Console.WriteLine(DataTableToCsv(dt));
+				}
+				else
+				{
+					WebClient wc = new WebClient();
+					wc.UploadString(url, result);
+				}
+
+
+				connection.Close();
+			}
+			else
+			{
+				Console.WriteLine(string.Format("ODBC Drive {0} does not exist", driver));
+			}
+		}
+
+		public static string DataTableToCsv(DataTable dt)
+		{
+			return ToCSV(dt);
+		}
+
+		public static string DataTableToJson(DataTable dt)
+		{
+			return DataTableToJSONWithStringBuilder(dt);
+		}
+
+		private static string getConnectionString(string driver)
+		{
+			string cs = "";
+			switch (driver.ToLower())
+			{
+				case "mysql":
+					string driverString = GetMySqlDriver();
+					cs = "Driver={{"+driverString+"}};Server={0};Database={1};Uid={2};Pwd={3};";
+					break;
+				case "postgres":
+					cs = "Driver={{PostgreSQL}};Server={0};Database={1};Uid={2};Pwd={3};";
+					break;
+				case "firebird":
+					cs = "Driver=Firebird/InterBase(r) driver;DBNAME={0}:{1};UID={2};PWD={3};";
+					break;
+				default:
+					cs = "Driver={{SQL Server}};Server={0};Database={1};Uid={2};Pwd={3};";
+					break;
+			}
+
+			return cs;
+		}
+
+		public static bool DriverExists(string driver)
+		{
+			if(!driverDict.ContainsKey(driver.ToLower()))
+			{
+				return false;
+			}
+
+			string key = driverDict[driver];
+
+			foreach (string d in driverList)
+			{
+				if(d.ToLower().Contains(key))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static string GetMySqlDriver()
+		{
+			string cs = "";
+
+			foreach (string d in driverList)
+			{
+				if (d.ToLower().Contains("mysql"))
+				{
+					return d;
+				}
+			}
+
+			return cs;
+		}
+
+		public static List<string> ListODBCDrivers()
+		{
+			//https://stackoverflow.com/questions/6457973/odbc-driver-list-from-net
+			Microsoft.Win32.RegistryKey reg = (Microsoft.Win32.Registry.LocalMachine).OpenSubKey("Software");
+			if (reg != null)
+			{
+				reg = reg.OpenSubKey("ODBC");
+				if (reg != null)
+				{
+					reg = reg.OpenSubKey("ODBCINST.INI");
+					if (reg != null)
+					{
+
+						reg = reg.OpenSubKey("ODBC Drivers");
+						if (reg != null)
+						{
+							foreach (string sName in reg.GetValueNames())
+							{
+								driverList.Add(sName);
+							}
+						}
+						try
+						{
+							reg.Close();
+						}
+						catch {}
+					}
+				}
+			}
+
+			return driverList;
+		}
+
+		private static string DataTableToJSONWithStringBuilder(DataTable table)
+		{
+			//https://www.c-sharpcorner.com/UploadFile/9bff34/3-ways-to-convert-datatable-to-json-string-in-Asp-Net-C-Sharp/
+			var JSONString = new StringBuilder();
+			if (table.Rows.Count > 0)
+			{
+				JSONString.Append("[");
+				for (int i = 0; i < table.Rows.Count; i++)
+				{
+					JSONString.Append("{");
+					for (int j = 0; j < table.Columns.Count; j++)
+					{
+						if (j < table.Columns.Count - 1)
+						{
+							JSONString.Append("\"" + table.Columns[j].ColumnName.ToString().Replace("\"", "\\\"") + "\":" + "\"" + table.Rows[i][j].ToString().Replace("\"", "\\\"") + "\",");
+						}
+						else if (j == table.Columns.Count - 1)
+						{
+							JSONString.Append("\"" + table.Columns[j].ColumnName.ToString().Replace("\"", "\\\"") + "\":" + "\"" + table.Rows[i][j].ToString().Replace("\"", "\\\"") + "\"");
+						}
+					}
+					if (i == table.Rows.Count - 1)
+					{
+						JSONString.Append("}");
+					}
+					else
+					{
+						JSONString.Append("},");
+					}
+				}
+				JSONString.Append("]");
+			}
+			return JSONString.ToString();
+		}
+
+		public static string ToCSV(DataTable dt)
+		{
+			string result = "";
+			for (int i = 0; i < dt.Columns.Count; i++)
+			{
+				result += dt.Columns[i];
+				if (i < dt.Columns.Count - 1)
+				{
+					result += ",";
+				}
+			}
+			
+			result += "\n";
+
+			foreach (DataRow dr in dt.Rows)
+			{
+				for (int i = 0; i < dt.Columns.Count; i++)
+				{
+					if (!Convert.IsDBNull(dr[i]))
+					{
+						string value = dr[i].ToString();
+						if (value.Contains(","))
+						{
+							value = String.Format("\"{0}\"", value.Replace("\"", "\\\""));
+							result += value;
+						}
+						else
+						{
+							result += dr[i].ToString();
+						}
+					}
+					if (i < dt.Columns.Count - 1)
+					{
+						result += ",";
+					}
+				}
+				result += "\n";
+			}
+
+			return result;
 		}
 	}
 }
